0bdf0f7979e4342b6658b44cd5c9d99785f483dc
Novimatrem / zoey Whitehat <thenovimatrem@protonmail.ch>
2023-06-27 11:24:14 +0000

    Update run-win-zoey.bat

--- run-win-zoey.bat
+++ run-win-zoey.bat
@@ -1,6 +1,8 @@
 @echo off
 cls
 @title bash text adventure Launcher
+REM the pings are just for timeout/sleep, a small wait
+@ping -n 1 -w 1000 0.0.0.1 > NUL 
 echo bash text adventure
 echo.
 echo Required dependencies: 
@@ -11,6 +13,7 @@ pause
 echo.
 echo Starting game...
 cd /D "%~dp0"
+REM the pings are just for timeout/sleep, a small wait
 @ping -n 3 -w 1000 0.0.0.1 > NUL 
 
 wt C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\wt.exe "C:\Program Files\Git\git-bash.exe" -c "bash textadv.sh"
