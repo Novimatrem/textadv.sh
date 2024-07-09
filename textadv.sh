266254761aeaf175be426c9331211bcf3a8a47cd
Novimatrem <TheNovimatrem@protonmail.ch>
2024-07-09 13:21:20 +0000

    v37a, graph alpha

--- textadv.sh
+++ textadv.sh
@@ -85,9 +85,9 @@ fi
 
 
 # release number and name
-DEVFULLRELEASENAME="textadv.sh - a bash text adventure by Novimatrem (version v36a)"
+DEVFULLRELEASENAME="textadv.sh - a bash text adventure by Novimatrem (version v37a)"
 
-MOJANGNAME="clear screen before zr"
+MOJANGNAME="graph"
 
 cd "$(dirname "$0")"
 clear
@@ -1365,6 +1365,57 @@ if [ "$QUERY" = "dev bcharart pixels" ]; then
   echo "-----------------------------"
   echo ""
   PRINTINFO=0
+fi
+
+if [ "$QUERY" = "dev bcharart graph" ]; then
+clear
+  qsim=devcheats?
+  PRINTINFO=0
+clear
+
+# terminal background color default:  |\e[40m
+
+  #echo -e "Set 2: |\e[100m \e[0m\e[101m \e[0m\e[102m \e[0m\e[103m \e[0m\e[104m \e[0m\e[105m \e[0m\e[106m \e[0m\e[107m \e[0m\e[108m|"
+  PRINTINFO=0
+  
+  
+# https://en.wikipedia.org/wiki/Box-drawing_characters
+  
+  clear
+echo -ne "                                                                                "
+echo -ne "                                                                                "
+echo -ne "    17│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " A  16│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " d  15│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " o  14│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " r  13│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " a  12│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " t  11│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " i  10│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " o   9│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " n   8│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne "     7│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " l   6│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " e   5│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " v   4│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " e   3│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " l   2│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne " s   1│\e[0m\e[101m       \e[40m \e[0m\e[102m       \e[40m \e[0m\e[103m       \e[40m \e[0m\e[104m       \e[40m \e[0m\e[107m       \e[40m \e[0m\e[105m       \e[40m                          "
+echo -ne "     0└───────────────────────────────────────────────────────────────────────   "
+echo -ne "      |Penis| |Pussy| |Booty| |Titty| |Belly| |Thigh|                           "
+
+while true; do
+    read -rep '                                                                                
+                                                              (Exit: q, Enter)  
+                                                                              ' yns
+    case $yns in
+        [Qq]* ) break;;
+        * ) true ;;
+    esac
+done
+clear
+
+  
 fi
 
 if [ "$QUERY" = "ts" ]; then
